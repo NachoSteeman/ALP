3b48d00372a4146d567b3acb654760c0667c3709
nachosteeman@gmail.com <nachosteeman@gmail.com>
2025-11-19 18:19:28 -0300

    Ejercicios 1 y 2

--- TP2/src/Simplytyped.hs
+++ TP2/src/Simplytyped.hs
@@ -20,13 +20,46 @@ import           Common
 -- conversion
 -----------------------
 
+
+
 -- conversion a términos localmente sin nombres
 conversion :: LamTerm -> Term
-conversion = undefined
+conversion (LVar x)         =  (Free (Global x))
+conversion (LAbs idx t m)  =  let term' = conversion m 
+                              in Lam t  (b2b (Global idx) 0 term') -- Lo casteo a Global a idx para que no tener error de tipos en b2b
+conversion (LApp m n)       =  (conversion m) :@: (conversion n)
+
+-- Seccion 8:
+conversion (LLet string m n) = Let (conversion m) (conversion n)
+
+-- Naturales:
+conversion (LZero)      = Zero
+conversion (LSuc n)     = Suc (conversion n)
+conversion (LRec n m p) = Rec (conversion n) (conversion m) (conversion p) 
+
+-- Listas:
+conversion (LNil)       = Nil 
+conversion (LCons n m)  = Cons (conversion n) (conversion m)
+
+
+-- b2b: Convierte de Brujin a Bound ?????????? VER
+b2b :: Name -> Int -> Term -> Term
+b2b idx i (Bound n)       = (Bound n)
+b2b idx i (Free name)     = if (idx == name) then (Bound i)
+                                             else (Free name)
+b2b idx i (t1 :@: t2)     = (b2b idx i t1) :@: (b2b idx i t2)
+b2b idx i (Lam t term) = Lam t (b2b idx (i+1) term)
+b2b idx i (Let t1 t2)     = Let (b2b idx i t1) (b2b idx (i+1) t2)
+b2b idx i Zero            = Zero
+b2b idx i (Suc n)         = Suc (b2b idx i n)
+b2b idx i (Rec n m p)     = Rec (b2b idx i n) (b2b idx i m) (b2b idx i p)
+b2b idx i Nil             = Nil
+b2b idx i (Cons n m)      = Cons (b2b idx i n) (b2b idx i m)
 
 ----------------------------
 --- evaluador de términos
 ----------------------------
+
 
 -- substituye una variable por un término en otro término
 sub :: Int -> Term -> Term -> Term
@@ -40,9 +73,39 @@ sub i t (Lam t'  u)           = Lam t' (sub (i + 1) t u)
 quote :: Value -> Term
 quote (VLam t f) = Lam t f
 
+quote (VNum NZero) = Zero
+quote (VNum (NSuc n)) = Suc (quote (VNum n))
+
+quote VNil = Nil
+quote (VCons n xs) = Cons (quote (VNum n)) (quote (VList xs))
+
 -- evalúa un término en un entorno dado
 eval :: NameEnv Value Type -> Term -> Value
-eval = undefined
+eval e (Bound i) = error ("Variable ligada fuera de alcance: " ++ show i)
+eval e (Free name )    = case lookup name e of
+    Just (v, _) -> v
+    Nothing     -> error ("Variable no definida: " ++ show name)
+
+eval e (Lam t term)    = VLam t term                                        
+--eval (NameEnv nvs t) (u :@: v)       = case (eval u) of 
+--                (VLam t term) -> eval e (sub 0 v term )
+
+-- Claudio: 
+eval e (t :@: u)     = let  (VLam _ body) = eval e t 
+                       in   eval e (sub 0 (quote (eval e u)) body)
+
+eval e Zero      = VNum NZero
+eval e (Suc t)   = case eval e t of
+                     VNum n -> VNum (NSuc n)
+                     _      -> error "Suc aplicado a no-numero"
+
+
+eval e Nil         = VList VNil
+eval e (Cons t u)  = VList (VCons (eval e t) (eval e u))
+
+
+
+
 
 
 
